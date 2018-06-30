f8120aca36ff99d4929dd42fe1344c12e1312638
Mircea Radu <radumircea88@gmail.com>
2018-06-30 18:56:00 +0300

    Create util.js

---
+++ util.js
@@ -0,0 +1,26 @@
+/*
+*  case: mobile toggle sets overflow: hidden on page container
+*  issue: scroll jumps to top of page
+*  set scroll manually to solve
+*/
+
+var util = {
+    mobileToggle: function ($container, $toggle, auto) {
+        $container.on("click", $toggle, function () {
+            if (auto) {
+                var $window = $(window),
+                    scroll = $window.scrollTop();
+                $(this).toggleClass("js--mobile");
+                $window.scrollTop(scroll);
+            } else {
+                $(this).toggleClass("js--mobile");
+            }
+        })
+    },
+    init: function () {
+        var _ = this;
+        _.mobileToggle($(".layout, body"), $(".header__toggle"), auto);
+    }
+}
+
+util.init();
